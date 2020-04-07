a8f98c6a0436c8d9565117a6df4005556af6dc25
Gonzalo Cuadra <gonzalocuadra@protonmail.com>
2020-04-07 11:18:01 -0400

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -5,25 +5,31 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (error, client) => {
-  assert.equal(error, null);
-
+MongoClient.connect(url).then((client) => {
   console.log('Connected correctly to server');
-
   const db = client.db(dbname);
-  dboper.insertDocument(db, { name: 'Vadonut', description: 'Test'}, 'dishes', (result) => {
+
+  dboper.insertDocument(db, { name: 'Vadonut', description: 'Test'}, 'dishes')
+  .then((result) => {
     console.log('Insert Document:\n', result.ops);
-    dboper.findDocuments(db, 'dishes', (docs) => {
-      console.log('Found Document:\n', docs);
-      dboper.updateDocument(db, {name: 'Vadonut'}, { description: 'Updated test' }, 'dishes', (result) => {
-        console.log('Updated Document:\n', result.result);
-        dboper.findDocuments(db, 'dishes', (docs) => {
-          console.log('Found Document:\n', docs);
-          db.dropCollection('dishes', (result) => {
-            console.log('Droped collection:\n', result);
-          })
-        });
-      });
-    });
-  });
-});
+    return dboper.findDocuments(db, 'dishes');
+  })
+  .then((docs) => {
+    console.log('Found Document:\n', docs);
+    return dboper.updateDocument(db, {name: 'Vadonut'}, { description: 'Updated test' }, 'dishes');
+  })
+  .then((result) => {
+    console.log('Updated Document:\n', result.result);
+    return dboper.findDocuments(db, 'dishes')
+  })
+  .then((docs) => {
+    console.log('Found Document:\n', docs);
+    return db.dropCollection('dishes');
+   })
+  .then((result) => {
+     console.log('Droped collection:\n', result);
+     client.close();
+  })
+  .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
